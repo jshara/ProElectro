61f1e97e622b69d9c7e8a4603e5c51e5e592c545
ivarawaico <ivarawaico@gmail.com>
2019-09-13 20:04:59 +1200

    Adding items
    Have added some more items to the list

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -41,6 +41,41 @@ class CategoriesTableSeeder extends Seeder
                 'cat_name' => 'Music Players',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'cat_name' => 'Speaker',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'cat_name' => 'Fridge',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'cat_name' => 'Headsets',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'cat_name' => 'Desktop Monitors',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'cat_name' => 'TV screens',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'cat_name' => 'Cameras',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'cat_name' => 'Cabels',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ]
         ]);
     }
