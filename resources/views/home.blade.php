828b68116d82793d56775df3ab5d2d1df3ace4fd
Ayesha Lata <ayesha.lata@fnu.ac.fj>
2019-09-16 13:12:45 +1200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,21 +11,21 @@
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="caurosal/headphones.jpg" class="d-block w-100px; h-25px" style="width: 2000px; height:700px"; alt="HeadPhone">
+        <img src="caurosal/headphones.jpg" class="d-block w-100px; h-25px" style="width: 100%; height:500px"; alt="HeadPhone">
         <div class="carousel-caption d-none d-md-block">
           <h5>Headphones</h5>
           <p></p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="caurosal/imac.png" class="d-block w-100px; h-25px" style="width: 2000px; height:700px"; alt="IMAC">
+        <img src="caurosal/imac.png" class="d-block w-100px; h-25px" style="width: 100%; height:500px"; alt="IMAC">
         <div class="carousel-caption d-none d-md-block">
           <h5>Apple Products</h5>
           <p></p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="caurosal/smart-watch.jpg" class="d-block w-100px; h-25px" style="width: 2000px; height:700px"; alt="SmartWatch">
+        <img src="caurosal/smart-watch.jpg" class="d-block w-100px; h-25px" style="width: 100%; height:500px"; alt="SmartWatch">
         <div class="carousel-caption d-none d-md-block">
           <h5>Smart Watch</h5>
           <p></p>
@@ -44,7 +44,7 @@
 </div>
 
 
-<p>Hello this is what you need to change</p>
+<p></p>
 <button class="btn btn-danger">
 click me
 </button>
