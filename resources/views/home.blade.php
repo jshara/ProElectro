b45691552cc1f352a9a30cee30a746fdacbbe41b
Ayesha Lata <ayesha.lata@fnu.ac.fj>
2019-09-18 07:05:55 +1200

    footer
    footer in layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,42 +46,5 @@
 </body>
 
 
-<!-- Footer -->
-<footer class="page-footer font-small special-color-dark pt-4">
 
-  <!-- Footer Elements -->
-  <div class="container">
-
-    <!-- Social buttons -->
-    <ul class="list-unstyled list-inline text-center">
-      <li class="list-inline-item">
-        <a class="btn-floating btn-fb mx-1"> <a href="https://www.facebook.com" target="_blank"><img alt="facebook"  src="https://img.icons8.com/color/48/000000/facebook-new.png" class="center"> </a>
-          <i class="fab fa-facebook-f"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-tw mx-1"> <a href="https://twitter.com" target="_blank"> <img alt="twitter" src="https://img.icons8.com/color/48/000000/twitter-circled.png" > </a> 
-          <i class="fab fa-twitter"> </i>
-        </a>
-      </li>
-            <li class="list-inline-item">
-        <a class="btn-floating btn-li mx-1"> <a href="https://linkedin.com" target="_blank"> <img alt="linkedin" src="https://img.icons8.com/color/48/000000/linkedin-circled.png"> 
-          <i class="fab fa-linkedin-in"> </i>
-        </a>
-     
-    </ul>
-    <!-- Social buttons -->
-
-  </div>
-  <!-- Footer Elements -->
-
- <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2019 Copyright:
-    <a href="www.proelctro.com"> ProElectro.com</a>
-  </div>
-  <!-- Copyright -->
-
-
-</footer>
-<!-- Footer -->
 @endsection
