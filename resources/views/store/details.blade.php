fdd1ac08e03f94e9f6b8c99d5095f18b5e31b1fb
Ayeshaf1j1 <54570006+Ayeshaf1j1@users.noreply.github.com>
2019-09-19 15:02:31 +1200

    Update details.blade.php

--- resources/views/store/details.blade.php
+++ resources/views/store/details.blade.php
@@ -11,6 +11,15 @@
        <br><br>
        The store is open {{$store->store_details}}.
        </p>
+
+       <!--Google map-->
+       <div id="map-container-google-9" class="z-depth-1-half map-container-5" style="height: 550px text=right">
+          <iframe src="https://maps.google.com/maps?q=Madryt&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+            style="border:0" allowfullscreen></iframe>
+        </div>
+
+<!--Google Maps-->
+
     </div>
 </div>
 
